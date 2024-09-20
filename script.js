fd3b6aeff2bceb0929172a0aefaba53a83e86b41
Alena-Sergeevna <94978479+Alena-Sergeevna@users.noreply.github.com>
2024-09-20 16:04:40 +0800

    Add files via upload
    test

---
+++ script.js
@@ -0,0 +1,374 @@
+const data = {
+    id: 123,
+    name: 'Разрабатывать дизайн-концепции веб-приложений в соответствии с корпоративным стилем заказчика',
+    type: 'competence',
+    childs: [
+        {
+            id: 124,
+            name: 'Разрабатывать интерфейс пользователя для веб-приложений с использованием современных стандартов.',
+            type: 'experience',
+            childs: [
+                {
+                    id: 125,
+                    name: 'Разрабатывать дизайн веб-приложений в соответствии со стандартами и требованиями заказчика.',
+                    type: 'experience',
+                    childs: [
+                        {
+                            id: 128,
+                            name: 'Формировать требования к дизайну веб-приложений.',
+                            type: 'experience',
+                            childs: []
+                        }
+                    ]
+                },
+                {
+                    id: 129,
+                    name: 'Разрабатывать интерфейс пользователя для веб-приложений с использованием современных стандартов.',
+                    type: 'able',
+                    childs: []
+                }
+            ]
+        },
+        {
+            id: 128,
+            name: 'Формировать требования к дизайну веб-приложений.',
+            type: 'experience',
+            childs: []
+        },
+        {id: 126, 
+            name: 'Разрабатывать прототип дизайна веб-приложения.', 
+            type: 'experience', 
+            childs: [
+                {
+                    id: 134,
+                    name: 'Стандарт UIX - UI &UXDesign.',
+                    type: 'know',
+                    childs: []
+                },
+                {
+                    id: 135,
+                    name: 'Придерживаться оригинальной концепции дизайна проекта и улучшать его визуальную привлекательность.',
+                    type: 'able',
+                    childs: [
+                        {
+                            id: 133,
+                            name: 'Способы создания эскиза, схем интерфейса и прототипа дизайна по предоставляемыминструкциям и спецификациям.',
+                            type: 'know',
+                            childs: []
+                        },
+                        {
+                            id: 134,
+                            name: 'Стандарт UIX - UI &UXDesign.',
+                            type: 'know',
+                            childs: []
+                        }
+                    ]
+                },
+                {
+                    id: 131,
+                    name: 'Разрабатывать эскизы веб-приложения.',
+                    type: 'experience',
+                    childs: [
+                        {
+                            id: 133,
+                            name: 'Способы создания эскиза, схем интерфейса и прототипа дизайна по предоставляемыминструкциям и спецификациям.',
+                            type: 'know',
+                            childs: []
+                        },
+                        {
+                            id: 134,
+                            name: 'Стандарт UIX - UI &UXDesign.',
+                            type: 'know',
+                            childs: []
+                        },
+                        {
+                            id: 135,
+                            name: 'Инструменты для разработки эскизов, схем интерфейсов и прототипа дизайна веб-приложений.',
+                            type: 'know',
+                            childs: []
+                        }
+                    ]
+                },
+                {
+                    id: 132,
+                    name: 'Разрабатывать схемы интерфейса веб-приложения.',
+                    type: 'experience',
+                    childs: [
+                        {
+                            id: 133,
+                            name: 'Способы создания эскиза, схем интерфейса и прототипа дизайна по предоставляемыминструкциям и спецификациям.',
+                            type: 'know',
+                            childs: []
+                        },
+                        {
+                            id: 134,
+                            name: 'Стандарт UIX - UI &UXDesign.',
+                            type: 'know',
+                            childs: []
+                        },
+                        {
+                            id: 135,
+                            name: 'Инструменты для разработки эскизов, схем интерфейсов и прототипа дизайна веб-приложений.',
+                            type: 'know',
+                            childs: []
+                        }
+                    ]
+                },
+            ]
+        },
+        {
+            id: 125,
+            name: 'Разрабатывать дизайн веб-приложений в соответствии со стандартами и требованиями заказчика.',
+            type: 'experience',
+            childs: [
+                {id: 126, 
+                    name: 'Разрабатывать прототип дизайна веб-приложения.', 
+                    type: 'experience', 
+                    childs: [
+                        {
+                            id: 134,
+                            name: 'Стандарт UIX - UI &UXDesign.',
+                            type: 'know',
+                            childs: []
+                        },
+                        {
+                            id: 135,
+                            name: 'Придерживаться оригинальной концепции дизайна проекта и улучшать его визуальную привлекательность.',
+                            type: 'able',
+                            childs: [
+                                {
+                                    id: 133,
+                                    name: 'Способы создания эскиза, схем интерфейса и прототипа дизайна по предоставляемыминструкциям и спецификациям.',
+                                    type: 'know',
+                                    childs: []
+                                },
+                                {
+                                    id: 134,
+                                    name: 'Стандарт UIX - UI &UXDesign.',
+                                    type: 'know',
+                                    childs: []
+                                }
+                            ]
+                        },
+                        {
+                            id: 131,
+                            name: 'Разрабатывать эскизы веб-приложения.',
+                            type: 'experience',
+                            childs: [
+                                {
+                                    id: 133,
+                                    name: 'Способы создания эскиза, схем интерфейса и прототипа дизайна по предоставляемыминструкциям и спецификациям.',
+                                    type: 'know',
+                                    childs: []
+                                },
+                                {
+                                    id: 134,
+                                    name: 'Стандарт UIX - UI &UXDesign.',
+                                    type: 'know',
+                                    childs: []
+                                },
+                                {
+                                    id: 135,
+                                    name: 'Инструменты для разработки эскизов, схем интерфейсов и прототипа дизайна веб-приложений.',
+                                    type: 'know',
+                                    childs: []
+                                }
+                            ]
+                        },
+                        {
+                            id: 132,
+                            name: 'Разрабатывать схемы интерфейса веб-приложения.',
+                            type: 'experience',
+                            childs: [
+                                {
+                                    id: 133,
+                                    name: 'Способы создания эскиза, схем интерфейса и прототипа дизайна по предоставляемыминструкциям и спецификациям.',
+                                    type: 'know',
+                                    childs: []
+                                },
+                                {
+                                    id: 134,
+                                    name: 'Стандарт UIX - UI &UXDesign.',
+                                    type: 'know',
+                                    childs: []
+                                },
+                                {
+                                    id: 135,
+                                    name: 'Инструменты для разработки эскизов, схем интерфейсов и прототипа дизайна веб-приложений.',
+                                    type: 'know',
+                                    childs: []
+                                }
+                            ]
+                        }
+                    ]
+                },
+            ]
+        },
+        {
+            id: 131,
+            name: 'Разрабатывать эскизы веб-приложения.',
+            type: 'experience',
+            childs: [
+                {
+                    id: 133,
+                    name: 'Способы создания эскиза, схем интерфейса и прототипа дизайна по предоставляемыминструкциям и спецификациям.',
+                    type: 'know',
+                    childs: []
+                },
+                {
+                    id: 134,
+                    name: 'Стандарт UIX - UI &UXDesign.',
+                    type: 'know',
+                    childs: []
+                },
+                {
+                    id: 135,
+                    name: 'Инструменты для разработки эскизов, схем интерфейсов и прототипа дизайна веб-приложений.',
+                    type: 'know',
+                    childs: []
+                }
+            ]
+        },
+        {
+            id: 132,
+            name: 'Разрабатывать схемы интерфейса веб-приложения.',
+            type: 'experience',
+            childs: [
+                {
+                    id: 133,
+                    name: 'Способы создания эскиза, схем интерфейса и прототипа дизайна по предоставляемыминструкциям и спецификациям.',
+                    type: 'know',
+                    childs: []
+                },
+                {
+                    id: 134,
+                    name: 'Стандарт UIX - UI &UXDesign.',
+                    type: 'know',
+                    childs: []
+                },
+                {
+                    id: 135,
+                    name: 'Инструменты для разработки эскизов, схем интерфейсов и прототипа дизайна веб-приложений.',
+                    type: 'know',
+                    childs: []
+                }
+            ]
+        }
+    ]
+};
+
+// Преобразование данных в иерархический формат для D3.js
+const root = d3.hierarchy(data, d => d.childs);
+
+const svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+const g = svg.append("g");
+
+// Добавляем zoom и pan (масштабирование и перемещение)
+svg.call(d3.zoom()
+    .scaleExtent([0.5, 3]) // Ограничение на масштабирование
+    .on("zoom", event => g.attr("transform", event.transform)));
+
+// Установка дерева и его размеров
+const treeLayout = d3.tree().size([2000, 2000]);
+
+treeLayout(root);
+
+// Определение маркеров для стрелок
+svg.append("defs").append("marker")
+    .attr("id", "arrow")
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 10)  // Подбираем корректную позицию стрелки относительно линии
+    .attr("refY", 0)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M0,-5L10,0L0,5")
+    .attr("class", "arrow");
+
+// Добавляем линии между узлами
+const link = g.append("g")
+    .selectAll(".link")
+    .data(root.links())
+    .enter().append("path")
+    .attr("class", "link")
+    .attr("d", d3.linkHorizontal()
+        .x(d => d.y)
+        .y(d => d.x))
+    .attr("marker-end", "url(#arrow)");
+
+// Функция для разбивки текста на строки
+function wrapText(text, width) {
+    const words = text.split(/\s+/).reverse();
+    let word;
+    const lines = [];
+    let line = [];
+
+    while (word = words.pop()) {
+        line.push(word);
+        const lineText = line.join(" ");
+        if (lineText.length > width) {
+            lines.push(line.slice(0, -1).join(" "));
+            line = [word];
+        }
+    }
+    lines.push(line.join(" "));
+    return lines;
+}
+
+// Добавляем узлы в виде прямоугольников с многострочным текстом
+const node = g.append("g")
+    .selectAll(".node")
+    .data(root.descendants())
+    .enter().append("g")
+    .attr("class", d => `node type-${d.data.type.replace(/\s+/g, '-')}`)
+    .attr("transform", d => `translate(${d.y},${d.x})`);
+
+node.append("rect")
+    .attr("width", 200)  // Увеличиваем ширину прямоугольника
+    .attr("height", d => {
+        const lines = wrapText(d.data.name, 35).length;  // Увеличиваем количество символов на строку
+        return 20 + lines * 14; // Высота зависит от количества строк
+    })
+    .attr("x", -100)  // Центрируем фигуру по оси X
+    .attr("y", d => {
+        const lines = wrapText(d.data.name, 35).length;  // Увеличиваем количество символов на строку
+        return - (lines * 7); // Центрируем по высоте
+    })
+    .attr("rx", 5)
+    .attr("ry", 5);
+
+node.append("text")
+    .attr("dy", d => {
+        const lines = wrapText(d.data.name, 35).length;  // Увеличиваем количество символов на строку
+        return -(lines - 1) * 7; // Сдвигаем текст вверх для центровки
+    })
+    .attr("text-anchor", "middle")
+    .selectAll("tspan")
+    .data(d => wrapText(d.data.name, 35))  // Увеличиваем количество символов на строку
+    .enter().append("tspan")
+    .attr("x", 0)
+    .attr("dy", "1.2em")
+    .text(d => d);
+
+// Добавляем возможность выделения дочерних элементов при клике
+node.on("click", selectNode);
+
+function selectNode(event, d) {
+    node.classed("selected", false);  // Снять выделение со всех узлов
+    node.classed("faded", false);  // Снять бледность со всех узлов
+    const selectedNodes = new Set();
+
+    function traverse(node) {
+        selectedNodes.add(node.data.id);
+        node.children && node.children.forEach(child => traverse(child));
+    }
+
+    traverse(d);  // Начать с выбранного узла
+
+    node.classed("selected", n => selectedNodes.has(n.data.id));  // Выделить связанные узлы
+    node.classed("faded", n => !selectedNodes.has(n.data.id));  // Сделать невыделенные узлы бледнее
+}
